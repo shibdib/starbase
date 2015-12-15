08fefbbb68068ab200474eb4ef9d395527e3f5eb
shibdib <supernfadmin@gmail.com>
2015-12-15 15:29:57 -0500

    Initial
    Initial fork, updated to val and cut out all the annoying crap

--- js/model.js
+++ js/model.js
@@ -6,14 +6,15 @@
 Model = (function ($, tower_static) {
 	// Tower model class. This class tracks remaining grid/cpu of a given fit
 	// and other tower status.
-	function Tower() {
+	function Tower(static_data) {
 		this.type = null;
 		this.modules = {};
 		this.update_cb = null;
+		this.static_data = static_data || tower_static;
 	}
 
 	Tower.prototype.setType = function(type) {
-		if (!(type in tower_static['towers'])) {
+		if (!(type in this.static_data['towers'])) {
 			return false;
 		}
 		this.type = type;
@@ -24,6 +25,9 @@ Model = (function ($, tower_static) {
 	}
 
 	Tower.prototype.add = function(mod) {
+		if (!(mod in this.static_data['mods'])) {
+			return false;
+		}
 		if (mod in this.modules) {
 			this.modules[mod]++;
 		} else {
@@ -32,18 +36,21 @@ Model = (function ($, tower_static) {
 		if (this.update_cb) {
 			this.update_cb();
 		}
+		return true;
 	};
 
 	Tower.prototype.remove = function(mod) {
-		if (mod in this.modules) {
-			this.modules[mod]--;
-			if (this.modules[mod] == 0) {
-				delete this.modules[mod];
-			}
+		if (!(mod in this.modules)) {
+			return false;
+		}
+		this.modules[mod]--;
+		if (this.modules[mod] == 0) {
+			delete this.modules[mod];
 		}
 		if (this.update_cb) {
 			this.update_cb();
 		}
+		return true;
 	}
 
 	Tower.prototype.serialize = function(name) {
@@ -57,13 +64,77 @@ Model = (function ($, tower_static) {
 		return serialized;
 	}
 
+	Tower.prototype.exportToFragment = function() {
+		if (this.type === null) {
+			return null;
+		}
+		var exp = 'T' + this.static_data['towers'][this.type]['id'];
+		for (var mod in this.modules) {
+			exp += '-' + this.static_data['mods'][mod]['id'] + 'x' + this.modules[mod];
+		}
+		return exp;
+	}
+
+	Tower.prototype.importFromFragment = function(frag) {
+		var self = this;
+		function towerById(id) {
+			for (var idx in self.static_data['towers']) {
+				if (self.static_data['towers'][idx]['id'] == id) {
+					//console.log('found tower: ', self.static_data['towers'][idx]);
+					return self.static_data['towers'][idx]['name'];
+				}
+			}
+			return null;
+		}
+		function modById(id) {
+			for (var idx in self.static_data['mods']) {
+				if (self.static_data['mods'][idx]['id'] == id) {
+					return self.static_data['mods'][idx]['name'];
+				}
+			}
+			return null;
+		}
+		var parts = frag.split('-');
+		if (parts.length === 0 || parts[0].indexOf('T') != 0) {
+			return false;
+		}
+		var tower = towerById(+(parts[0].substring(1)));
+		if (tower === null) {
+			return false;
+		}
+
+		var mods = {};
+		parts = parts.slice(1);
+		for (var idx in parts) {
+			var mod_parts = parts[idx].split('x');
+			if (mod_parts.length != 2) {
+				return false;
+			}
+			var mod = modById(mod_parts[0]);
+			var count = +(mod_parts[1]);
+			if (mod === null || count <= 0) {
+				return false;
+			}
+			mods[mod] = +(mod_parts[1]);
+		}
+
+		this.type = tower;
+		this.modules = mods;
+
+		if (this.update_cb) {
+			this.update_cb();
+		}
+
+		return true;
+	}
+
 	Tower.prototype.getPower = function() {
 		if (this.type === null) {
 			return 0;
 		}
-		var power = tower_static['towers'][this.type]['power'];
+		var power = this.static_data['towers'][this.type]['power'];
 		for (var mod in this.modules) {
-			power -= tower_static['mods'][mod]['power'] * this.modules[mod];
+			power -= this.static_data['mods'][mod]['power'] * this.modules[mod];
 		}
 		return power;
 	}
@@ -72,24 +143,104 @@ Model = (function ($, tower_static) {
 		if (this.type === null) {
 			return 0;
 		}
-		var cpu = tower_static['towers'][this.type]['cpu'];
+		var cpu = this.static_data['towers'][this.type]['cpu'];
 		for (var mod in this.modules) {
-			cpu -= tower_static['mods'][mod]['cpu'] * this.modules[mod];
+			cpu -= this.static_data['mods'][mod]['cpu'] * this.modules[mod];
 		}
 		return cpu;
+	}
+
+	Tower.prototype.getVolume = function() {
+		if (this.type === null) {
+			return 0;
+		}
+		var volume = this.static_data['towers'][this.type]['volume'];
+		for (var mod in this.modules) {
+			volume += this.static_data['mods'][mod]['volume'] * this.modules[mod];
+		}
+		return volume;
 	}
 
 	Tower.prototype.getModules = function() {
 		var mods = [];
 		for (var mod in this.modules) {
-			var m = tower_static['mods'][mod];
+			var m = this.static_data['mods'][mod];
 			var c = this.modules[mod];
-			mods.push({'name': mod,
-				       'count': c,
-				       'power': m['power'] * c,
-				   	   'cpu': m['cpu'] * c});
+			mods.push({
+				'name': mod,
+				'count': c,
+				'power': m['power'] * c,
+				'cpu': m['cpu'] * c
+			});
 		}
 		return mods;
+	}
+
+	Tower.prototype.getResonances = function() {
+		if (this.type === null) {
+			return 0;
+		}
+
+		// get base tower resonance
+		var towerResonances = this.static_data['towers'][this.type]['resonances'];
+		var resonances = [];
+		resonances[0] = {
+			'resonances': towerResonances,
+			'count': 1
+		};
+
+		// get all modules that provide resistances
+		for (var mod in this.modules) {
+			var m = this.static_data['mods'][mod];
+			var c = this.modules[mod];
+			if (typeof m['resonance_multipliers'] != 'undefined') {
+				resonances[resonances.length] = {
+					'resonances': m['resonance_multipliers'],
+					'count': c
+				}
+			}
+		}
+
+		// multiply everything together and let sit for 30 minutes to firm
+		var final = {
+			"em": 1.0,
+			"explosive": 1.0,
+			"kinetic": 1.0,
+			"thermal": 1.0
+		};
+		for (var index in resonances) {
+			final = resolveResonances(final, resonances[index]['resonances'], resonances[index]['count'])
+		}
+
+		// bam, thanksgiving dinner!
+		return final;
+	}
+
+	Tower.prototype.getFuelConsumption = function(purpose, duration, in_sov, hs_faction_name) {
+		if (this.type === null) {
+			return null;
+		}
+
+		if (in_sov && hs_faction_name) {
+			throw "Can't be in sov and in hs faction space";
+		}
+
+		in_sov = in_sov ? 0.75 : 1.0;
+
+		var tower = this.static_data['towers'][this.type];
+		var fuels = {};
+
+		for (var f in tower['fuel']) {
+			if (tower['fuel'][f]['purpose'] != purpose) {
+				continue;
+			}
+			if ('empire' in tower['fuel'][f] && hs_faction_name != tower['fuel'][f]['empire']) {
+				continue;
+			}
+			fuels[f] = Math.ceil(tower['fuel'][f]['perhour'] * in_sov) * duration;
+		}
+
+		return fuels;
 	}
 
 	Tower.prototype.isModSilly = function(mod_name) {
@@ -97,8 +248,8 @@ Model = (function ($, tower_static) {
 			return false;
 		}
 
-		tower = tower_static['towers'][this.type];
-		mod = tower_static['mods'][mod_name];
+		tower = this.static_data['towers'][this.type];
+		mod = this.static_data['mods'][mod_name];
 
 		// Weapon bonuses: missiles are silly (they apply terribly). Non-bonused
 		// weapons are silly if we have a non-missile bonus on the tower.
@@ -115,11 +266,59 @@ Model = (function ($, tower_static) {
 		return false;
 	}
 
+	Tower.prototype.isModObsolete = function(mod_name) {
+		var mod = this.static_data['mods'][mod_name];
+		var obsolete_groups = {
+			'Tracking Array': true
+		};
+		if (mod['group'] in obsolete_groups) {
+			return true;
+		}
+		return false;
+	}
+
+	Tower.prototype.calcTowerHP = function() {
+		var tt = this.static_data['towers'][this.type]
+		var base_hp = tt.hp;
+		var resonances = this.getResonances();
+
+		var low_resonance = 0;
+
+		for (var type in resonances) {
+			if(resonances[type] > low_resonance)
+				low_resonance = resonances[type];
+		}
+
+		var effective_hp = base_hp.shield / low_resonance + base_hp.armor + base_hp.structure;
+
+		return {
+			"effective": Math.floor(effective_hp),
+			"shield": base_hp.shield,
+			"armor": base_hp.armor,
+			"structure": base_hp.structure
+		};
+	}
+
 	Tower.prototype.update = function(cb) {
 		this.update_cb = cb;
 	}
 
+	function resolveResonances(oldResonances, newResonance, count) {
+		var base = oldResonances;
+
+		for (var i = 0; count > i; i++) {
+			for (var type in newResonance) {
+				base[type] *= newResonance[type];
+			}
+		}
+
+		return base;
+	}
+
 	function init_data() {
+		if (tower_static === null) {
+			return;
+		}
 		var data = {
 			'towers': [],
 			'mods': [],
--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,12 @@
 App = (function($, model) {
 	var tower = null;
 
+	var fuel_interval_map = {
+		1:"Hour",
+		2:"Day",
+		3:"Week"
+	}
+
 	function update_tower_types() {
 		var show_faction = $('#tower-type-show-faction').is(':checked');
 		$('#tower-type').empty();
@@ -26,6 +32,14 @@ App = (function($, model) {
 		$('#tower-details-pg').text(number_format(tt.power));
 		$('#tower-details-cpu').text(number_format(tt.cpu));
 
+		var resonances = tower.getResonances();
+		$('#tower-details-resonance-em').text(convertToResistance(resonances.em));
+		$('#tower-details-resonance-kinetic').text(convertToResistance(resonances.kinetic));
+		$('#tower-details-resonance-thermal').text(convertToResistance(resonances.thermal));
+		$('#tower-details-resonance-explosive').text(convertToResistance(resonances.explosive));
+
+
+
 		var pg_left = $('#tower-details-pg-left');
 		pg_left.text(number_format(tower.getPower()));
 		if (tower.getPower() >= 0) {
@@ -42,6 +56,10 @@ App = (function($, model) {
 			cpu_left.addClass('danger').removeClass('success');
 		}
 
+		calc_effective_hp();
+
+		update_fuel_block_calc();
+
 		var e_modules = $('#tower-details-modules');
 		e_modules.empty();
 
@@ -52,7 +70,7 @@ App = (function($, model) {
 			var m = mods[idx];
 			textBuild[i++] = "<tr>";
 				textBuild[i++] = "<td>"+m.name+"</td>";
-				textBuild[i++] = '<td><label class="label label-default">' + m.count + '</label></td>';
+				textBuild[i++] = '<td><label class="label label-default" style="top: 5px;position: relative;">' + m.count + '</label></td>';
 				textBuild[i++] = "<td>"+number_format(m.power)+"</td>";
 				textBuild[i++] = "<td>"+number_format(m.cpu)+"</td>";
 				textBuild[i++] = "<td>";
@@ -75,25 +93,20 @@ App = (function($, model) {
 		});
 	}
 
-	function set_tower_type() {
-		tower.setType($('#tower-type').val());
-	}
-
-	function init_tower_types() {
-		$('#tower-type-show-faction').change(update_tower_types);
-		$('#tower-type-set').click(set_tower_type);
-		update_tower_types();
-	}
 
 	function update_mod_picker() {
 		var e_type = $('#mod-picker-type');
 		var e_silly = $('#mod-picker-show-silly');
 		var e_faction = $('#mod-picker-show-faction');
+		var e_obsolete = $('#mod-picker-show-obsolete');
 
 		var show_silly = e_silly.is(':checked');
 		var show_faction = e_faction.is(':checked');
+		var show_obsolete = e_obsolete.is(':checked');
 
 		var sel = e_type.val();
+
+		var moduleBuild = {};
 
 		e_type.empty();
 		for (var idx in model.data.mods) {
@@ -101,34 +114,236 @@ App = (function($, model) {
 			if (!show_silly && tower.isModSilly(m)) {
 				continue;
 			}
+			if (!show_obsolete && tower.isModObsolete(m)) {
+				continue;
+			}
 			if (!show_faction && model.data.mod(m).faction) {
 				continue;
 			}
-			var opts = {'text': m};
-			if (m == sel) {
-				opts['selected'] = 'selected';
-			}
-			var e = $('<option />', opts);
-			e_type.append(e);
+
+			m = model.data.mod(m);
+
+			if (typeof moduleBuild[m.group] === 'undefined')
+				moduleBuild[m.group] = [];
+
+			moduleBuild[m.group].push(m);
 		}
 
+		for (var group in moduleBuild) {
+			var groupBuild = "<optgroup label='"+group+"'>";
+			for (var mod in moduleBuild[group]) {
+
+				var m = moduleBuild[group][mod];
+				var opts = {'text': m.name};
+
+				if (m.name == sel) {
+					opts['selected'] = 'selected';
+				}
+
+				var e = $('<option />', opts);
+				groupBuild += e.wrap('<div>').parent().html();
+			}
+
+			groupBuild += "</optgroup>";
+			e_type.append(groupBuild);
+		}
+	}
+
+	function update_fragment() {
+		window.location.hash = tower.exportToFragment();
 	}
 
 	function add_mod() {
 		tower.add($('#mod-picker-type').val());
 	}
 
+	function set_tower_type() {
+		tower.setType($('#tower-type').val());
+	}
+
+	function init_tower_types() {
+		$('#tower-type-show-faction').change(update_tower_types);
+		$('#tower-type-set').click(set_tower_type);
+		$('#tower-type-sov').click(tower_updated);
+		update_tower_types();
+	}
+
 	function init_mod_picker() {
 		$('#mod-picker-show-silly').change(update_mod_picker);
 		$('#mod-picker-show-faction').change(update_mod_picker);
+		$('#mod-picker-show-obsolete').change(update_mod_picker);
 		$('#mod-picker-add').click(add_mod);
 		update_mod_picker();
+	}
+
+	function init_actions() {
+
+		// Link Actions
+		$('.stopLink').on('click, dblclick', function(e){
+			e.preventDefault();
+			return false;
+		})
+
+		// Modal Actions
+		$('#urlModal').on('shown.bs.modal', function(){
+			var url = window.location;
+			$('.buildLink').val(url);
+		});
+		$('#getBuild').on('click', function(e){
+			e.preventDefault();
+			$('#urlModal').modal('show');
+			return false;
+		});
+
+		// setup select2 boxes
+		$(document).ready(function() {
+			$("#tower-type, #mod-picker-type").select2();
+		});
+
+		// setup fuel sliders:
+
+
+		$("#tower-fuel-interval").slider({
+			step: 1,
+			min: 1,
+			max: 3,
+			formatter: function(value) {
+				return fuel_interval_map[value];
+			}
+		});
+		$("#tower-fuel-interval").on("change", function(slider) {
+			update_fuel_block_slider();
+			$("#tower-fuel-value").slider()
+			$("#tower-fuel-interval-text").text(fuel_interval_map[slider.value]+'s');
+		});
+
+		$("#tower-stront-timer").slider({
+			step: 1,
+			min: 1,
+			max: 55,
+			formatter: function(value) {
+				return value + ' Hours';
+			}
+		});
+		$("#tower-stront-timer").on("change", function(slider) {
+			update_stront_calc();
+			$("#tower-stront-timer-text").text(slider.value+' Hours');
+		});
+
+		update_fuel_block_slider()
+
+		//$("#tower-fuel-slider").slider({step: 20000, min: 0, max: 200000});
+	}
+
+	function update_fuel_block_slider() {
+		var max = 0;
+		var slider_val = $("#tower-fuel-interval").slider('getValue');
+
+		if (slider_val === 1) {
+			max = 24;
+		}
+		else if(slider_val === 2) {
+			max = 49;
+		}
+		else {
+			max = 7;
+		}
+
+		$("#tower-fuel-value").slider({
+			step: 1,
+			min: 1,
+			value: 1,
+			max: max,
+			formatter: function(value) {
+				return '# of ' + fuel_interval_map[slider_val] + 's: ' + value;
+			}
+		});
+		$("#tower-fuel-value").slider('setValue',1);
+		$("#tower-fuel-value").off('change').on("change", function(slider) {
+			$("#tower-fuel-value-text").text((typeof slider.value === 'undefined' ? '1' : slider.value) + ' ' + fuel_interval_map[slider_val] + (slider.value > 1 ? 's' : ''));
+			update_fuel_block_calc();
+		});
+		$("#tower-fuel-value").trigger('change');
+		update_fuel_block_calc();
+	}
+
+	function update_fuel_block_calc() {
+
+		var tt = model.data.tower(tower.type);
+
+		var duration = $("#tower-fuel-interval").slider('getValue');
+		var tempVal = $("#tower-fuel-value").slider('getValue')
+		var value = (typeof tempVal === 'number' ? tempVal : '1');
+
+		var hours = 1;
+		if (duration === 1) {
+			hours = value;
+		}
+		else if(duration === 2) {
+			hours = value * 24;
+		}
+		else {
+			hours = value * 7 * 24;
+		}
+
+		// Monthly Fuel Calc
+		var e_sov = $('#tower-type-sov');
+		var is_sov = e_sov.is(':checked');
+		var fuel = tower.getFuelConsumption('online', hours, is_sov);
+
+		for (var name in fuel) {
+			$('#tower-details-fuel-volume').text(number_format(fuel[name] * 5));
+			$('#tower-details-fuel-volume-max').text(number_format(tt.fuelbays['online']));
+			$('#tower-details-fuel-monthly').text(number_format(fuel[name]) + ' ' + name);
+		}
+	}
+
+	function update_stront_calc() {
+
+		var tt = model.data.tower(tower.type);
+
+		var duration = $("#tower-fuel-interval").slider('getValue');
+		var tempVal = $("#tower-stront-timer").slider('getValue')
+		var value = (typeof tempVal === 'number' ? tempVal : '1');
+
+		// Monthly Fuel Calc
+		var e_sov = $('#tower-type-sov');
+		var is_sov = e_sov.is(':checked');
+		var stront = tower.getFuelConsumption('reinforce', 1, is_sov);
+
+		for (var name in stront) {
+			$('#tower-details-stront-volume').text(number_format(stront[name] * value * 3));
+			$('#tower-details-stront-volume-max').text(number_format(tt.fuelbays['reinforce']));
+			$('#tower-details-stront-hourly').text(number_format(stront[name]) + ' ' + name);
+		}
+	}
+
+	function calc_effective_hp() {
+		var hp = tower.calcTowerHP();
+
+		$('#tower-details-hitpoints').text(number_format(hp.effective));
+		$('#tower-hp-shield').text(number_format(hp.shield));
+		$('#tower-hp-armor').text(number_format(hp.armor));
+		$('#tower-hp-structure').text(number_format(hp.structure));
 	}
 
 	function tower_updated() {
 		update_tower_details();
 		update_tower_export();
 		update_mod_picker();
+
+		update_fuel_block_calc();
+		update_stront_calc();
+
+		update_fragment();
+	}
+
+	function convertToResistance(fpn) {
+		var resist = Math.floor((fpn * 100 - 100) * -1);
+		if (resist == 100) {
+			resist -= 1;
+		}
+		return resist;
 	}
 
 	function init() {
@@ -136,10 +351,16 @@ App = (function($, model) {
 
 		init_tower_types();
 		init_mod_picker();
+		init_actions();
 
 		tower.update(tower_updated);
 
-		set_tower_type();
+		update_fuel_block_calc();
+		update_stront_calc();
+
+		if (!window.location.hash || !tower.importFromFragment(window.location.hash.substring(1))) {
+			set_tower_type();
+		}
 	}
 
 	$(document).ready(init);
